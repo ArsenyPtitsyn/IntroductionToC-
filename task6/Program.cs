8df7b3277eb7be3e44115bf6ed39fd5d91e22912
Arseny Ptitsyn <senyabird@yandex.ru>
2022-10-17 18:17:08 +0300

    task6 completed

--- task6/Program.cs
+++ task6/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool IsEven(int number)
+{
+    return number % 2 == 0;
+}
+
+Console.WriteLine("Число 4 " + (IsEven(4) ? "чётно" : "нечётно"));
+Console.WriteLine("Число -3 " + (IsEven(-3) ? "чётно" : "нечётно"));
+Console.WriteLine("Число 7 " + (IsEven(7) ? "чётно" : "нечётно"));
