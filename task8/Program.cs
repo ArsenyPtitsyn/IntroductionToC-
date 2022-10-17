c1a934fb8a4ac2c050d38650227b6627d276339f
Arseny Ptitsyn <senyabird@yandex.ru>
2022-10-17 18:46:20 +0300

    task8 completed

--- task8/Program.cs
+++ task8/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void PrintEvenNumbersLessOrEqualN(int n)
+{
+    int index = 2;
+    while (index <= n)
+    {
+        Console.Write(index + "|");
+        index += 2;
+    }
+    Console.WriteLine();
+}
+
+PrintEvenNumbersLessOrEqualN(5);
+PrintEvenNumbersLessOrEqualN(8);
