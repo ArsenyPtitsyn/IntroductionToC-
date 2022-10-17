764ce4451ca9d89dfccb462c6854135af5fe1c48
Arseny Ptitsyn <senyabird@yandex.ru>
2022-10-17 17:35:28 +0300

    task2 completed

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿int FindMax(int a, int b)
+{
+    if (a > b) return a;
+    else return b;
+}
+
+Console.WriteLine("Большее из чисел 5 и 7: " + FindMax(5, 7));
+Console.WriteLine("Большее из чисел 2 и 10: " + FindMax(2, 10));
+Console.WriteLine("Большее из чисел -9 и -3: " + FindMax(-9, -3));
