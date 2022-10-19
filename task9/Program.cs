0f821465b149369ea8fbb6498aee7d477e85f729
Arseny Ptitsyn <senyabird@yandex.ru>
2022-10-19 22:14:48 +0300

    task9* completed

--- task9/Program.cs
+++ task9/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool IsMultiple(int number)
+{
+    return (number % 7 == 0 && number % 23 == 0);
+}
+
+Console.WriteLine("число 14 " + (IsMultiple(14) ? "ЯВЛЯЕТСЯ" : "НЕ ЯВЛЯЕТСЯ") + " одновременно кратным 7 и 23.");
+Console.WriteLine("число 46 " + (IsMultiple(46) ? "ЯВЛЯЕТСЯ" : "НЕ ЯВЛЯЕТСЯ") + " одновременно кратным 7 и 23.");
+Console.WriteLine("число 161 " + (IsMultiple(161) ? "ЯВЛЯЕТСЯ" : "НЕ ЯВЛЯЕТСЯ") + " одновременно кратным 7 и 23.");
