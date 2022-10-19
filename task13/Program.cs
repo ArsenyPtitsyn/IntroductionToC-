68fc3c6af16655cf846f5299929aa0c11f3e5352
Arseny Ptitsyn <senyabird@yandex.ru>
2022-10-19 21:31:09 +0300

    task 13 fixed, task 15 completed

--- task13/Program.cs
+++ task13/Program.cs
@@ -1,23 +1,13 @@
-﻿int CountOfDigits(int number)
+﻿void ThirdDigitOfNumber(int number)
 {
-    int counter = 0;
-    while (number - number % Math.Pow(10, counter) != 0)
-    {
-        counter++;
-    }
-    return counter;
-}
-
-void ThirdDigitOfNumber(int number)
-{
-    if (CountOfDigits(number) < 3)
-    {
-        Console.WriteLine("У этого числа нет третьей цифры");
-    }
+    if (number < 100) Console.WriteLine("У числа нет третьей цифры");
     else
     {
-        int countOfDigits = CountOfDigits(number);
-        Console.WriteLine("Третья цифра числа: " + number / (int)Math.Pow(10, countOfDigits - 3) % 10);
+        while (number > 1000)
+        {
+            number /= 10;
+        }
+        Console.WriteLine("Третья цифра числа равна " + number % 10);
     }
 }
 
--- task15/Program.cs
+++ task15/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool IsWeekend(int number)
+{
+    if (number == 1) return false;
+    if (number == 2) return false;
+    if (number == 3) return false;
+    if (number == 4) return false;
+    if (number == 5) return false;
+    if (number == 6) return true;
+    if (number == 7) return true;
+
+    else throw new Exception("Нет дня с таким числом!");
+}
+
+Console.WriteLine("6 -> " + (IsWeekend(6) ? "Да" : "Нет"));
+Console.WriteLine("7 -> " + (IsWeekend(7) ? "Да" : "Нет"));
+Console.WriteLine("1 -> " + (IsWeekend(1) ? "Да" : "Нет"));
