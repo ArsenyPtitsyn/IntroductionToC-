e44df56617b5e0643f659d7b3195fb29802da936
Arseny Ptitsyn <senyabird@yandex.ru>
2022-10-22 14:12:14 +0300

    Added exception handling

--- task15/Program.cs
+++ task15/Program.cs
@@ -1,16 +1,16 @@
 ﻿bool IsWeekend(int number)
 {
-    if (number == 1) return false;
-    if (number == 2) return false;
-    if (number == 3) return false;
-    if (number == 4) return false;
-    if (number == 5) return false;
-    if (number == 6) return true;
-    if (number == 7) return true;
+    if (number >= 1 && number <= 5) return false;
+    if (number == 6 || number == 7) return true;
 
     else throw new Exception("Нет дня с таким числом!");
 }
 
-Console.WriteLine("6 -> " + (IsWeekend(6) ? "Да" : "Нет"));
-Console.WriteLine("7 -> " + (IsWeekend(7) ? "Да" : "Нет"));
-Console.WriteLine("1 -> " + (IsWeekend(1) ? "Да" : "Нет"));
+try
+{
+    Console.WriteLine("14 -> " + (IsWeekend(14) ? "Да" : "Нет"));
+}
+catch
+{
+    Console.WriteLine("Нет дня с таким числом!");
+}
