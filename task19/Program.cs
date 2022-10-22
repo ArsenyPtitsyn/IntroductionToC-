5e51596525501f0267047b1823c74a37c18cbc2b
Arseny Ptitsyn <senyabird@yandex.ru>
2022-10-22 18:16:56 +0300

    task19 completed

--- task19/Program.cs
+++ task19/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool IsPalindrome(int number)
+{
+    if (number > 9999 && number < 100000)
+    {
+        return (number % 10 == number / 10000
+        && number / 10 % 10 == number / 1000 % 10);
+    }
+    else
+    {
+        throw new System.Exception("Вы ввели не пятизначное число");
+    }
+}
+
+try
+{
+    Console.WriteLine("14212 -> " + (IsPalindrome(14212) ? "Да" : "Нет"));
+    Console.WriteLine("12821 -> " + (IsPalindrome(12821) ? "Да" : "Нет"));
+    Console.WriteLine("23432 -> " + (IsPalindrome(23432) ? "Да" : "Нет"));
+}
+catch
+{
+    Console.WriteLine("Вы ввели не пятизначное число");
+}
