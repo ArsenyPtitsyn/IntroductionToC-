7af1f57b75bbbcb04d920c9796369224586862ef
Arseny Ptitsyn <senyabird@yandex.ru>
2022-10-22 20:26:11 +0300

    task21 completed

--- task21/Program.cs
+++ task21/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double distanceBetweenTwoPointsIn3DSpace(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    return Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
+}
+
+Console.WriteLine("Расстояние между точками A(3, 6, 8) и B(2, 1, -7) в трёхмерном пространстве равно "
++ $"{Math.Round(distanceBetweenTwoPointsIn3DSpace(3, 6, 8, 2, 1, -7), 2)}");
+Console.WriteLine("Расстояние между точками A(7, -5, 0) и B(1, -1, 9) в трёхмерном пространстве равно "
++ $"{Math.Round(distanceBetweenTwoPointsIn3DSpace(7, -5, 0, 1, -1, 9), 2)}");
