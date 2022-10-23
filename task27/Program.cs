6d348034322a70fdc5ada185b4a6a052dd03ea53
Arseny Ptitsyn <senyabird@yandex.ru>
2022-10-23 19:31:23 +0300

    task27 completed

--- task27/Program.cs
+++ task27/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿int CountOfTheDigitsOfTheNumber(int num)
+{
+    int countOfDigits = 0;
+
+    while (num != 0)
+    {
+        countOfDigits++;
+        num /= 10;
+    }
+    return countOfDigits;
+}
+
+int SumOfTheDigitsOfTheNumber(int number)
+{
+    int result = 0;
+    int countOfDigits = CountOfTheDigitsOfTheNumber(number);
+    for (int i = 0; i < countOfDigits; i++)
+    {
+        result += number % 10;
+        number /= 10;
+    }
+    return result;
+}
+
+Console.WriteLine("Сумма цифр числа 452 -> " + SumOfTheDigitsOfTheNumber(452));
+Console.WriteLine("Сумма цифр числа 82 -> " + SumOfTheDigitsOfTheNumber(82));
+Console.WriteLine("Сумма цифр числа 9012 -> " + SumOfTheDigitsOfTheNumber(9012));
