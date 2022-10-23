32410750f802d0e715a10b392b10530e57599da5
Arseny Ptitsyn <senyabird@yandex.ru>
2022-10-23 22:32:39 +0300

    task29 completed

--- task29/Program.cs
+++ task29/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿int[] FillArray()
+{
+    Console.Write("Введите любое количество целых чисел через зяапятую: ");
+
+    string integersString = Console.ReadLine();
+    string[] integers = integersString.Split(",");
+
+    int[] ints = new int[integers.Length];
+    for (int i = 0; i < ints.Length; i++)
+    {
+        ints[i] = Convert.ToInt32(integers[i]);
+    }
+    return ints;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.Write(array[array.Length - 1] + "]");
+}
+
+int[] array = FillArray();
+PrintArray(array);
