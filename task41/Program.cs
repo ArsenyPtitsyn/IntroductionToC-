60e48bb5f6183a980c10d15fc5cf83fdc079a5e3
Arseny Ptitsyn <senyabird@yandex.ru>
2022-11-16 20:22:23 +0300

    Task41 completed

--- task41/Program.cs
+++ task41/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿int[] CreateAnArrayOfIntegersFromUserEnteredData()
+{
+    Console.Write("Введите целые числа через запятую без пробела: ");
+    string stringOfIntegers = Console.ReadLine();
+    string[] arrayOfStringsOfIntegers = stringOfIntegers.Split(",");
+
+    int[] arrayOfIntegers = new int[arrayOfStringsOfIntegers.Length];
+    for (int i = 0; i < arrayOfIntegers.Length; i++)
+    {
+        arrayOfIntegers[i] = Convert.ToInt32(arrayOfStringsOfIntegers[i]);
+    }
+    return arrayOfIntegers;
+}
+
+int NumberOfPositiveNumbersInArray(int[] array)
+{
+    int counter = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) counter++;
+    }
+    return counter;
+}
+
+int[] array = CreateAnArrayOfIntegersFromUserEnteredData();
+Console.WriteLine($"Количество положительных чисел, введённых пользователем равно: {NumberOfPositiveNumbersInArray(array)}");
