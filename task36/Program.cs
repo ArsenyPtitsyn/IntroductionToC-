53db595f6c024ebc59e7493ffb8cab3bf9074a54
Arseny Ptitsyn <senyabird@yandex.ru>
2022-11-16 17:28:26 +0300

    Task 36 completed

--- task36/Program.cs
+++ task36/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FillArray(int[] array)
+{
+    var rnd = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(-999, 1000);
+    }
+}
+
+int sumOfArrayElementsWithOddIndexes(int[] array)
+{
+    int result = 0;
+
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        result += array[i];
+    }
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+
+    Console.WriteLine(array[array.Length - 1] + "]");
+}
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine($"Sum of array elements with odd indexes is: {sumOfArrayElementsWithOddIndexes(array)}");
