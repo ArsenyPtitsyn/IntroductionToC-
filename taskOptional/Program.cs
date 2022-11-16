8491a2f39e17d466a67512e121cad8a0560216b8
Arseny Ptitsyn <senyabird@yandex.ru>
2022-11-16 19:08:14 +0300

    Task optional completed

--- taskOptional/Program.cs
+++ taskOptional/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int FindIndexOfArrayElementWithDesiredValue(int[] array, int value)
+{
+    int index = -1;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == value)
+        {
+            index = i;
+            break;
+        }
+    }
+    return index;
+}
+
+void FillArray(int[] array)
+{
+    var rnd = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(-999, 1000); // целые числа [-999; 1000).
+    }
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+
+    Console.WriteLine(array[array.Length - 1] + "]");
+}
+
+// int[] array = new int[10];
+// FillArray(array);
+// PrintArray(array);
+int[] array1 = { 25, 303, -24562, 2645, 5834561, 4, 274352, 4, 4 };
+PrintArray(array1);
+Console.WriteLine($"Find the index of an array element with the desired value is:{FindIndexOfArrayElementWithDesiredValue(array1, 4)}");
