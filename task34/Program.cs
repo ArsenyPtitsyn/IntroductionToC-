7d0d9fffba52006f9686ae4e5334fb7ad73ee859
Arseny Ptitsyn <senyabird@yandex.ru>
2022-11-16 17:03:33 +0300

    Task 34 completed

--- task34/Program.cs
+++ task34/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FillArray(int[] array)
+{
+    var rnd = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(100, 1000);
+    }
+}
+
+int CountOfEvenNumbersInArray(int[] array)
+{
+    int counter = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) counter++;
+    }
+    return counter;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+
+    Console.WriteLine(array[array.Length - 1] + "]");
+}
+
+int[] array = new int[15];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine($"The number of even numbers in the array is: {CountOfEvenNumbersInArray(array)}");
