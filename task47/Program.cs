e126261cd9429d0e1bd5a92160ab028cff416f2a
Arseny Ptitsyn <senyabird@yandex.ru>
2022-11-19 18:24:36 +0300

    task47 completed

--- task47/Program.cs
+++ task47/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿double[,] CreateTwoDimensionalArray(int rows, int cols, double maxValue, double minValue)
+{
+    double[,] array = new double[rows, cols];
+
+    var rnd = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            array[i, j] = rnd.NextDouble() * (maxValue - minValue) + minValue;
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] array = CreateTwoDimensionalArray(5, 3, -10, 10);
+PrintArray(array);
