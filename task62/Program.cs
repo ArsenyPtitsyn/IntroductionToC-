7be52a903d482006cbafdbfd352cea28ca852ea9
Arseny Ptitsyn <senyabird@yandex.ru>
2022-11-24 23:22:09 +0300

    task62 completed

--- task62/Program.cs
+++ task62/Program.cs
@@ -1,17 +1,38 @@
-﻿int[,] Fill4x4ArraySpirally()
+﻿int[,] CreateSpirallyArray(int rows, int cols)
 {
-    int[,] array = {{1, 2, 3, 4},
-                    {12, 13, 14, 5},
-                    {11, 16, 15, 6},
-                    {10, 9, 8, 7}};
+    int[,] array = new int[rows, cols];
+
+    int row = 0, col = 0; // Индексы элемента, с которого мы начинаем заполнение.
+    int dx = 1, dy = 0; // Направление, в котором мы начинаем заполнение массива.
+    int directionChanges = 0; // Количество поворотов, случившихся при заполнении массива.
+    int NumberOfElBeforeRotation = cols; // Количество элементов до поворота.
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[row, col] = i + 1;
+        if (--NumberOfElBeforeRotation == 0)
+        {
+            NumberOfElBeforeRotation = cols * (directionChanges % 2)
+            + rows * ((directionChanges + 1) % 2) - (directionChanges / 2 - 1) - 2;
+            int temp = dx;
+            dx = -dy;
+            dy = temp;
+            directionChanges++;
+        }
+        col += dx;
+        row += dy;
+    }
     return array;
 }
 
 void PrintArray(int[,] array)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+    int rows = array.GetLength(0);
+    int cols = array.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
+        for (int j = 0; j < cols; j++)
         {
             Console.Write(array[i, j] + "\t");
         }
@@ -19,5 +40,5 @@ void PrintArray(int[,] array)
     }
 }
 
-int[,] array = Fill4x4ArraySpirally();
+int[,] array = CreateSpirallyArray(7, 9);
 PrintArray(array);
