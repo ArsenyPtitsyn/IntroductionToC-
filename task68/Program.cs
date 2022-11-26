56256e2910bb95a573727f4aeeb57c94d03fab7c
Arseny Ptitsyn <senyabird@yandex.ru>
2022-11-26 15:39:08 +0300

    task68 completed

--- task68/Program.cs
+++ task68/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿/*Задача 68: Напишите программу вычисления функции Аккермана 
+с помощью рекурсии. Даны два неотрицательных числа m и n.
+*/
+int AkkermanFunction(int a, int b)
+{
+    if (a == 0) { return b + 1; }
+    if (b == 0) { return AkkermanFunction(a - 1, 1); }
+    return AkkermanFunction(a - 1, AkkermanFunction(a, b - 1));
+}
+
+int res = AkkermanFunction(2, 3);
+Console.WriteLine(res);
