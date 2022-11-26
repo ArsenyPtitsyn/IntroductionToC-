0932526868789cf246f39116df35b1134c0525c9
Arseny Ptitsyn <senyabird@yandex.ru>
2022-11-26 14:16:40 +0300

    task64 completed

--- task64/Program.cs
+++ task64/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿/*
+Задача 64: Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа в промежутке 
+от N до 1. Выполнить с помощью рекурсии.
+*/
+void PrintNumbersdescending(int n)
+{
+    if (n == 0) { return; }
+
+    Console.Write($"{n} \t");
+    PrintNumbersdescending(n - 1);
+}
+PrintNumbersdescending(10);
