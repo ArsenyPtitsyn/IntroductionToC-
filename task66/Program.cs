833bd85464bc76136d85ae6add38fbdac9d5c777
Arseny Ptitsyn <senyabird@yandex.ru>
2022-11-26 14:45:15 +0300

    task66 completed

--- task66/Program.cs
+++ task66/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая 
+найдёт сумму натуральных элементов в промежутке от M до N.
+*/
+int FindSumOfIntegersBetween(int lowerLimit, int upperLimit)
+{
+    if (lowerLimit > upperLimit) { return 0; }
+    return lowerLimit + FindSumOfIntegersBetween(lowerLimit + 1, upperLimit);
+}
+
+int sum = FindSumOfIntegersBetween(-15, 5);
+Console.WriteLine(sum);
